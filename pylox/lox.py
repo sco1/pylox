cf94fe6d8a9571a9585ec001041e0533db0c8397
S. Co1 <sco1.git@gmail.com>
2021-10-26 19:37:56 -0400

    Frame initial entry points

---
+++ pylox/lox.py
@@ -0,0 +1,45 @@
+import typing as t
+from pathlib import Path
+
+import typer
+from rich import print
+from rich.prompt import Prompt
+
+pylox_cli = typer.Typer()
+Prompt.prompt_suffix = " "
+
+
+class Lox:
+    def __init__(self) -> None:
+        ...
+
+    def run_file(self, src_filepath: Path) -> None:
+        src = src_filepath.read_text()
+        self.run(src)
+
+    def run_prompt(self) -> None:
+        while True:
+            line = Prompt.ask(">>>")
+            self.run(line)
+
+    def run(self, src: str) -> None:
+        print(f"[red](fake)[/red] Running source: '{src}'")
+
+
+@pylox_cli.command()
+def main(lox_script: t.Optional[Path] = typer.Argument(default=None)) -> None:
+    """
+    Welcome to the pylox Lox interpreter!
+
+    If a path to a Lox file is not provided, a pylox REPL will be opened.
+    """
+    lox = Lox()
+    if not lox_script:
+        # REPL
+        lox.run_prompt()
+    else:
+        lox.run_file(lox_script)
+
+
+if __name__ == "__main__":
+    pylox_cli()
